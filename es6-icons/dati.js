74a735bb11f25f26064d05d6189e1b1df2ca376f
Lorenzo Bernini <templarec@gmail.com>
2021-03-22 19:14:06 +0100

    Milestone 2 ok

--- es6-icons/dati.js
+++ es6-icons/dati.js
@@ -101,7 +101,21 @@ $(document).ready(() => {
 	icons.forEach((elemento) => {
 		let template = $('.template li').clone();
 		let tag = `<i class="${elemento.family} ${elemento.prefix}${elemento.name}"></i>`;
-		template.children('span#icon').append(tag);
+		//Milestone 2
+		color = "";
+		switch (elemento.type) {
+			case "animal":
+				color = "blue";
+				break;
+			case "vegetable":
+				color ="#FFA500"
+				break;
+			case "user"	:
+				color = "#800080"
+				break;
+
+		}
+		template.children('span#icon').append(tag).css("color", color);
 		template.children('span#desc').text(`${elemento.name}`);
 		$('#animals').append(template);
 	});
